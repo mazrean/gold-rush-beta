52208d3bc1829fafc1d8938731ae89756a847c9d
mazrean <shunsuke.w04@gmail.com>
2021-03-12 01:17:25 +0900

    disable keepalive

--- api/api.go
+++ api/api.go
@@ -2,8 +2,10 @@ package api
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/mazrean/gold-rush-beta/openapi"
 )
@@ -20,8 +22,25 @@ func Setup() {
 				URL: fmt.Sprintf("http://%s:8000", os.Getenv("ADDRESS")),
 			},
 		},
-		HTTPClient: http.DefaultClient,
-		Debug:      false,
+		HTTPClient: &http.Client{
+			Transport: &http.Transport{
+				DialContext: (&net.Dialer{
+					Timeout:   30 * time.Second,
+					KeepAlive: 30 * time.Second,
+					DualStack: true,
+				}).DialContext,
+				ForceAttemptHTTP2:     true,
+				MaxIdleConns:          100,
+				MaxConnsPerHost:       100,
+				IdleConnTimeout:       90 * time.Second,
+				TLSHandshakeTimeout:   10 * time.Second,
+				ResponseHeaderTimeout: 10 * time.Second,
+				ExpectContinueTimeout: 1 * time.Second,
+				DisableKeepAlives:     true,
+			},
+			Timeout: 60 * time.Second,
+		},
+		Debug: false,
 	})
 	api = client.DefaultApi
 }
