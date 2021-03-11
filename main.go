14200a1ebe62de31ee154de0b72a49a122110b62
mazrean <shunsuke.w04@gmail.com>
2021-03-11 22:12:27 +0900

    優先度修正

--- main.go
+++ main.go
@@ -95,6 +95,32 @@ func schedule(ctx context.Context) {
 					fmt.Printf("cash\n")
 					cash(ctx, arg)
 					continue
+				default:
+				}
+
+				if time.Since(startTime).Minutes() < 5 {
+					select {
+					case arg := <-exploreChan:
+						//fmt.Printf("explore\n")
+						explore(ctx, arg)
+						continue
+					default:
+					}
+				} else {
+					select {
+					case arg := <-licenseChan:
+						//fmt.Printf("license\n")
+						license(ctx, arg)
+						continue
+					default:
+					}
+				}
+
+				select {
+				case arg := <-cashChan:
+					fmt.Printf("cash\n")
+					cash(ctx, arg)
+					continue
 				case arg := <-digChan:
 					dig(ctx, arg)
 					continue
