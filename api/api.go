5c097436d3ea98862b0b94f0d58531dbf80ff234
mazrean <shunsuke.w04@gmail.com>
2021-03-15 22:40:47 +0900

    client変更

--- api/api.go
+++ api/api.go
@@ -2,11 +2,30 @@ package api
 
 import (
 	"fmt"
+	"net"
 	"net/http"
 	"os"
+	"time"
 )
 
 var (
 	baseURL = fmt.Sprintf("http://%s:8000", os.Getenv("ADDRESS"))
-	client  = http.DefaultClient
+	client  = &http.Client{
+		Transport: &http.Transport{
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+				DualStack: true,
+			}).DialContext,
+			ForceAttemptHTTP2:     true,
+			MaxIdleConns:          100,
+			MaxConnsPerHost:       100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ResponseHeaderTimeout: 10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			DisableKeepAlives:     true,
+		},
+		Timeout: 60 * time.Second,
+	}
 )
